9088e5141a204e2770abe6c93ee6f7a692d712c1
elkin-pinto <elkin.pinto.7e7@itb.cat>
2023-10-10 11:58:48 +0200

    Signo sodaquial

--- tot/kotlin/pfgfagg.kt
+++ tot/kotlin/pfgfagg.kt
@@ -1,25 +1,65 @@
 import java.util.*
 fun main() {
     var scan = Scanner(System.`in`)
-    // busquem el numero de voltes i crear variables per després. 
-    var voltes = scan.nextInt()
-    // agafarem la vida del monstre, i l'atac de tot dos campeons
-    var vida = 0
-    var twitch = 0
-    var rammus = 0
-    var n_voltes = 0
-    var guanyador = "RAMMUS"
-    for (i in 1..voltes) { // donarem tantes voltes com digui l'usuari
-        vida = scan.nextInt()
-        rammus = scan.nextInt()
-        twitch = scan.nextInt()
-        while (vida > 0){ // Treurem el dany de la vida del monstre fins a que sigui 0 o menos i direm qui el va matar el les rondes que van tardar.
-            vida = vida - rammus - twitch
-            n_voltes++
+    // Definim el scanner per fer-ho servir
+    // Fem un bucle que llegira cada input i determinara si es valid
+    var boolean = true
+    var vueltas = 0
+    var dia:Int = 0
+    var mes:Int = 0
+    var any:Int = 0
+    while (boolean && vueltas != 3) {
+        vueltas ++
+        when (vueltas) {
+            1 -> print("Introdueixi el any: ")
+            2 -> print("Introdueixi el mes: ")
+            3 -> print("Introdueixi el dia: ")
         }
-        if ((vida + twitch) > 0) guanyador = "TWITCH" 
-        println("$guanyador $n_voltes") // Fem el print i reiniciem les variables.
-        n_voltes = 0
-        guanyador = "RAMMUS"
+        if (scan.hasNextInt()) {
+            var input = scan.nextInt()
+            if (vueltas == 1) {
+                if (input in 0..2023) {any = input}
+                else {
+                    println("Error")
+                    vueltas--
+                }
+            }
+            if (vueltas == 2) {
+                if (input in 1..12) {mes = input}
+                else {
+                    println("Error")
+                    vueltas--
+                }
+            }
+            if (vueltas == 3) {
+                when {
+                    mes in intArrayOf(4,6,9,11) && input in 1..30 -> dia = input
+                    (mes == 2) && input in 1..28 || any % 4 == 0 && input in 1..29 && any % 100 != 0 || any % 400 == 0 -> dia = input
+                    mes in intArrayOf(1,3,5,7,8,10,12) && input in 1..31 -> dia = input
+                    else -> {println("Error"); vueltas--}
+                }
+            }  
+        }
+        else {
+            println("Error")
+            vueltas--
+            scan.next()
+        }
     }
+    when {
+        mes == 1 && dia > 19 || mes == 2 && dia < 19 -> println("El teu signe del sodiac és: Acuario")
+        mes == 2 && dia > 18 || mes == 3 && dia < 21 -> println("El teu signe del sodiac és: Piscis")
+        mes == 3 && dia > 20 || mes == 4 && dia < 20 -> println("El teu signe del sodiac és: Aries")
+        mes == 4 && dia > 19 || mes == 5 && dia < 21 -> println("El teu signe del sodiac és: Tauro")
+        mes == 5 && dia > 20 || mes == 6 && dia < 21 -> println("El teu signe del sodiac és: Géminis")
+        mes == 6 && dia > 20 || mes == 7 && dia < 23 -> println("El teu signe del sodiac és: Cáncer")
+        mes == 7 && dia > 22 || mes == 8 && dia < 23 -> println("El teu signe del sodiac és: Leo")
+        mes == 8 && dia > 22 || mes == 9 && dia < 23 -> println("El teu signe del sodiac és: Virgo")
+        mes == 9 && dia > 22 || mes == 10 && dia < 23 -> println("El teu signe del sodiac és: Libra")
+        mes == 10 && dia > 22 || mes == 11 && dia < 22 -> println("El teu signe del sodiac és: Escorpio")
+        mes == 11 && dia > 21 || mes == 12 && dia < 22 -> println("El teu signe del sodiac és: Sagitario")
+        mes == 12 && dia > 21 || mes == 1 && dia < 20 -> println("El teu signe del sodiac és: Capricornio")
+    }
+    
+
 }
--- tot/kotlin/tempCodeRunnerFile.kt
+++ tot/kotlin/tempCodeRunnerFile.kt
@@ -1,18 +1,1 @@
-import java.util.*
-fun main() {
-    var scan = Scanner(System.`in`)
-    // busquem el numero de voltes i crear variables per després. 
-    var voltes = scan.nextInt()
-    var dies:Int
-    var infectats:Int
-    var alarma:String? = null
-    for (i in 1..voltes) { // donarem tantes voltes com digui l'usuari
-        dies = scan.nextInt() // agafarem un numero per cada volta del bucle
-        alarma = "OK"
-        for (i in 1..dies) { // farem tants dies com digui l'usuari
-            infectats = scan.nextInt()
-            if (infectats > 299) alarma = "ALARMA" // Si la condicio es certa sera una alarma
-        }
-        println(alarma)
-    }
-}
+var resto = numero % 2
