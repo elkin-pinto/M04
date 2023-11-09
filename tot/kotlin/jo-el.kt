10b6d60214f11c2032435e5fbebb41fdb4630247
elkin-david <elkin.pinto.7e7@itb.cat>
2023-11-09 21:14:06 +0100

    bcb

--- tot/kotlin/jo-el.kt
+++ tot/kotlin/jo-el.kt
@@ -2,11 +2,25 @@ import java.util.*
 
 fun main() {
     var scan = Scanner(System.`in`)
-    var vueltas = scan.nextInt()
-    
-    repeat(vueltas) {
-        var casos = scan.nextInt()
-        var arrayStrings = arrayOf<Int>({repeat(26) {scan.nextInt()}})
-        
+    var Nmatriu = scan.nextInt()
+    var Narray = scan.nextInt()
+    var matriu = Array(Nmatriu) {Array(Narray) {0}}
+
+
+    for (i in matriu.indices) {
+        for (e in 0..Narray - 1){
+            matriu[i][e] = scan.nextInt()
+        }
     }
-} 
+    var numero_a_comprobar = scan.nextInt()
+    var suma_fila = 0
+    var suma_columna = 0
+    var respuesta = "-1 -1"
+    for (i in matriu.indices) {
+        for (e in 0..Narray - 1){
+            if (e == numero_a_comprobar) suma_columna += matriu[i][e]
+            if (i == numero_a_comprobar) suma_fila += matriu[i][e]
+        }
+    }
+    println("$suma_fila $suma_columna")
+}
--- tot/kotlin/tempCodeRunnerFile.kt
+++ tot/kotlin/tempCodeRunnerFile.kt
@@ -1,1 +1,23 @@
-arrayNumeros += scan.nextInt()
+import java.util.*
+
+fun main() {
+    var scan = Scanner(System.`in`)
+    var Nmatriu = scan.nextInt()
+    var Narray = scan.nextInt()
+    var matriu = Array(Nmatriu) {Array(Narray) {0}}
+
+
+    for (i in matriu.indices) {
+        for (e in 0..Narray - 1){
+            matriu[i][e] = scan.nextInt()
+        }
+    }
+    var numero_a_comprobar = scan.nextInt()
+    var numero_a_cambiar = scan.nextInt()
+    for (i in matriu.indices) {
+        for (e in 0..Narray - 1){
+            if (numero_a_cambiar == matriu[i][e]) print("$numero_a_cambiar ")
+            else print("${matriu[i][e]} ")
+        }
+    }
+}
